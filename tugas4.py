b1e2a52f6ebcf3506e65c0b0e96ab7939a65b402
Afrizanhidayat06 <afrizanhidayat06@gmail.com>
2024-10-30 17:09:24 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,4 @@
+print("nama saya afrizan hidayat")
+print("nim sy 24241188")
+print("sy berasal dari kopang")
+print("jumlah dari 2 digit nim pertama sy di tambah 2 digit nim terahir sy adalah[24 + 88=112]")
